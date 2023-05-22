6b8dafaae47d26795add1841d1cff02e314cf944
mac <mac@macs-MacBook-Pro.local>
2023-05-22 16:11:42 +0100

    LightMode and dark mode

--- src/theme.js
+++ src/theme.js
@@ -109,13 +109,105 @@ blueAccent: {
         blueAccent: {
             100: "#151632",
             200: "#2a2d64",
-            700: "#3e4396",
-            600: "#535ac8",
+            300: "#3e4396",
+            400: "#535ac8",
             500: "#6870fa",
-            400: "#868dfb",
-            300: "#a4a9fc",
-            200: "#c3c6fd",
-            100: "#e1e2fe",
+            600: "#868dfb",
+            700: "#a4a9fc",
+            800: "#c3c6fd",
+            900: "#e1e2fe",
         },
     }),    
 }); 
+
+// mui theme settings
+export const themeSettings = (mode) => {
+    const colors = tokens(mode);
+
+    return {
+        palette: {
+            mode: mode,
+            ...(mode === "dark"
+            ?{
+                primary: {
+                    main: colors.primary[500],
+                },
+                secondary:{
+                    main: colors.greenAccent[500],
+                },
+                nuetral: {
+                    dark:colors.grey[700],
+                    main:colors.grey[500],
+                    light:colors.grey[100]
+            },
+            background:{
+                default: colors.primary[500],
+            }
+        }:{
+            primary: {
+                main: colors.primary[100],
+            },
+            secondary:{
+                main: colors.greenAccent[500],
+            },
+            nuetral: {
+                dark: colors.grey[700],
+                main: colors.grey[500],
+                light:colors.light[100],
+        },
+        background:{
+            default: "#fcfcfc"
+        }
+    }),
+        }, 
+        typography:{
+            fontFamily: ["source sans pro", "sans-serif"].join(","),
+            fontsize: 12,
+            h1:{
+                fontFamily: ["source sans pro", "sans-serif"].join(","),
+                fontsize: 40,
+            },
+            h2:{
+                fontFamily: ["source sans pro", "sans-serif"].join(","),
+                fontsize: 32,
+            },
+            h3:{
+                fontFamily: ["source sans pro", "sans-serif"].join(","),
+                fontsize: 24,
+            },
+            h4:{
+                fontFamily: ["source sans pro", "sans-serif"].join(","),
+                fontsize: 20,
+            },
+            h5:{
+                fontFamily: ["source sans pro", "sans-serif"].join(","),
+                fontsize: 16,
+            },
+            h6:{
+                fontFamily: ["source sans pro", "sans-serif"].join(","),
+                fontsize: 14,
+            },
+            },
+
+            };
+        };
+        
+     //context for the color mode   
+   export const ColorModeContext = createContext({
+    toggleColorMode: () => {}
+   });
+
+   export const useMode =() =>{
+    const [mode, setMode] = useState("dark");
+    
+    const colorMode = useMemo(
+        ()=>({
+            toggleColorMode :() =>
+            setMode((prev) => (prev === "light" ? "dark" :"light")),
+        }),
+        []
+    );
+    const theme =useMemo(() => createTheme(themeSettings(mode)), [mode])
+        
+    return [theme, colorMode];
+}
